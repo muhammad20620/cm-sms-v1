50a55dc12bdf7b8eb8c64256304125d28093dc35
wakkarkhan <28950237+wakkarkhan@users.noreply.github.com>
2026-02-11 12:08:41 +0500

    testing

--- routes/web.php
+++ routes/web.php
@@ -55,8 +55,6 @@ Route::get('teacher/account-disable', function () {
     return view('teacher.account_disable');
 })->name('teacher.account_disable');
 
-
-
 Route::get('librarian/account-disable', function () {
     return view('librarian.account_disable');
 })->name('librarian.account_disable');
